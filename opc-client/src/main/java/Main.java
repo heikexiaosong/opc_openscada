28eafb5c1bb09ca6565cf67b80fd451798506f1a
Qi Song <heikexiaosong@gmail.com>
2018-05-08 11:48:19 +0800

    关闭初始化日志信息

--- opc-client/src/main/java/Main.java
+++ opc-client/src/main/java/Main.java
@@ -1,43 +1,61 @@
 import org.jinterop.dcom.common.JIException;
+import org.jinterop.dcom.common.JISystem;
 import org.openscada.opc.lib.common.ConnectionInformation;
 import org.openscada.opc.lib.da.*;
 
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // create connection information
+
+        // 关闭初始化日志信息
+        Logger.getLogger ( "org.jinterop" ).setLevel(Level.WARNING);
+
         final ConnectionInformation ci = new ConnectionInformation();
         ci.setHost("localhost");
         ci.setDomain("");
         ci.setUser("opc");
         ci.setPassword("opc");
+
         //ci.setProgId("Matrikon.OPC.Simulation");
         ci.setClsid("f8582cf2-88fb-11d0-b850-00c0f0104305"); // if ProgId is not working, try it using the Clsid instead
-        final String itemId = "Random.UInt4";
-        // create a new server
-        final Server server = new Server(ci, Executors.newSingleThreadScheduledExecutor());
 
+        final Server server = new Server(ci, Executors.newSingleThreadScheduledExecutor());
         try {
-            // connect to server
             server.connect();
+
             // add sync access, poll every 500 ms
             final AccessBase access = new SyncAccess(server, 500);
-            access.addItem(itemId, new DataCallback() {
+
+            access.addItem("Random.UInt4", new DataCallback() {
                 @Override
                 public void changed(Item item, ItemState state) {
                     try {
-                        System.out.println(item.getGroup().getName() + ": " + state.getValue().getObjectAsUnsigned().getValue());
+                        System.out.println(state.getTimestamp().getTime() + " ==> " + item.getGroup().getName() + ", " + item.getId() + "]: " +  state.getValue().getObjectAsUnsigned().getValue());
                     } catch (JIException e) {
                         e.printStackTrace();
                     }
                 }
             });
+
+            access.addItem("Random.String", new DataCallback() {
+                @Override
+                public void changed(Item item, ItemState state) {
+                    try {
+                        System.out.println(state.getTimestamp().getTime() + " ==> [" + item.getGroup().getName() + ", " + item.getId() + "]: " + state.getValue().getObjectAsString2());
+                    } catch (JIException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
             // start reading
             access.bind();
             // wait a little bit
-            Thread.sleep(2 * 1000);
+            Thread.sleep(100 * 1000);
             // stop reading
             access.unbind();
         } catch (final JIException e) {
